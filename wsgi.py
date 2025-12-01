ba2564ea8e0bdfd0cfad6533533a63012846b08b
nanoxtremertx-gif <nanoxtremertx@gmail.com>
2025-12-01 18:41:46 -0300

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-# wsgi.py
-import os
-import sys
-# Asegurarse de que el directorio de la aplicación esté en el path de Python
-sys.path.insert(0, os.path.dirname(__file__)) 
-from servidor3 import app as application
-
-if __name__ == "__main__":
-    application.run()
